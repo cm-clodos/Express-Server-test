8f952cd5770a7908913950aa167e6dd7806e4151
Claudia <clodos_m92@hotmail.com>
2022-05-26 14:08:45 +0200

    uuid generator hinzugefügt inkl folder erstellen

--- server.mjs
+++ server.mjs
@@ -3,12 +3,16 @@
 import express from "express"
 import fs from "fs"
 import fileUpload from "express-fileupload"
+import {v4 as uuidv4} from 'uuid'
+import axios from "axios"
 
 // own modules
 import {validFiletyp} from "./service.mjs";
 
 const server = express()
 const port = 8080
+//https://stackoverflow.com/questions/25209073/sending-multiple-responses-with-the-same-response-object-in-express-js
+//verbindung aktiv halten
 
 //express wandelt JSON nicht automatisch um! Lösung: Middleware das JSON parst to js bevor die daten in der funktion bearbeitet werden.
 server.use(express.json())
@@ -24,14 +28,18 @@ server.use(fileUpload(
  * @author: Claudia
  * Note: params include filetype
  */
-server.get("/download/:filename", (req, res) => {
+
+server.get("/download", (req, res) => {
     //request = incoming data
     //response = outgoing data
     // let message = req.body.message
     //console.log(message)
-    const filename = req.params.filename
-    const pathDown = `./download/${filename}`
-    const pathUp = `./upload/${filename}`
+    const uuid = req.body.uuid
+    const filename = req.body.filename
+    console.log(uuid)
+    console.log(filename)
+    const pathDown = `./download/${uuid}/${filename}` //download /(uuid) 5465736/ filename
+    const pathUp = `./upload/${uuid}/${filename}`
 
 
 
@@ -39,14 +47,27 @@ server.get("/download/:filename", (req, res) => {
 
         if (!err) {
             console.log(`${filename} exists in ${pathDown}`)
-            res.status(200).download(pathDown, () => {
+            res.status(200).download(pathDown, async () => {
                 try {
-                    fs.unlink(pathUp, () => {
+                    // löscht zuerst die files im folder
+                    await fs.unlink(pathUp, () => {
                         console.log("File deleted in upload")
+
                     })
-                    fs.unlink(pathDown, () => {
+                    await fs.unlink(pathDown, () => {
                         console.log("File deleted in download")
+
                     })
+                    // löscht die folder uuid
+                   await fs.rmdir("./upload/" + uuid, () => {
+                        console.log("folder deleted")
+                    })
+
+                   await fs.rmdir("./download/" + uuid, () => {
+                        console.log("folder deleted")
+                    })
+
+
                 } catch (err) {
                     console.log("Error message: ", err)
 
@@ -58,6 +79,8 @@ server.get("/download/:filename", (req, res) => {
             return res.status(404).send({message: "File Not Found!"})
         }
     })
+
+
 
 })
 /**
@@ -85,18 +108,57 @@ server.post("/upload", async (req, res) => {
                 //Use the name of the input field (i.e. "uploadFile") to retrieve the uploaded file
                 let filename = req.files.uploadFile.name
                 let uploadFile = req.files.uploadFile
+                let uuid = uuidv4()
+                try{
+                    fs.mkdir('./upload/' + uuid, { recursive: true }, (err) => {
+                        if (err) throw err;
+                    });
+                }catch (err){
+                    console.log(err)
+                }
 
                 //Use the mv() method to place the file in upload directory (i.e. "uploads")
-                await uploadFile.mv('./upload/' + filename);
+                await uploadFile.mv('./upload/'+ uuid +'/' + filename);
 
-                res.status(200).send({message: "File Upload successfully"})
+
+                res.status(200).send({message: "File Upload successfully", uuid: uuid})
+
                 console.log("file uploaded")
+
 
             }
         } catch (err) {
             console.log(err)
         }
 
+})
+let key = "6d7dc944-6931-db59-b9d3-e5d3a24e44b3:fx"
+
+server.get('/key-usage',  (req, res) => {
+    //innerhalb ein httprequest auf deepl machen
+    //'https://api-free.deepl.com/v2/usage?auth_key=' + key
+    /*
+    let key = req.query.key
+    console.log(key)
+    axios.post('https://api-free.deepl.com/v2/usage', {
+        auth_key: "6d7dc944-6931-db59-b9d3-e5d3a24e44b3:fx",
+    }).then(res => {
+        console.log("statuscode: " + res.status)
+        console.log(res)
+    })
+
+
+     */
+
+    /*
+    let charCount = res.body.character_count
+    let charLimit = res.body.character_limit
+
+    console.log("Character count: " + charCount)
+    console.log("Character limit: " + charLimit)
+    console.log(key)
+
+     */
 })
 
 
--- service.mjs
+++ service.mjs
@@ -1,16 +1,78 @@
+import fs from "fs"
+import path from "path"
 
 
 let filenameValid = "text.ini"
 let filenameInvalid = "text.doc"
 
 
-
 export function validFiletyp(filename){
-    let validTypes = ["txt", "ini"];
-    let filetyp = filename.split(".").pop();
 
-    let valid = validTypes.includes(filetyp)
-    return valid
+    let validTypes = [".txt", ".ini"]
+    return validTypes.includes(path.extname(filename.toLowerCase()))
 }
 
-console.log(validFiletyp(filenameValid))
+
+
+//console.log(validFiletyp(filenameValid))
+
+/**
+ * Read the file line by line, trim and split the rows
+ * @autor Claudia
+ * @param path {String}
+ * @param splitter {fn}
+ * @returns {string[]}
+ */
+function splitFilesInRows(path,splitter ){
+    let KeyValue = []
+
+    try {
+        // read content of the File
+        const data = fs.readFileSync(path, {encoding:"utf-8"})
+
+        //split the content by new line (achtung lines mit leeren Strings)
+        const  lines = data.trim().split(/\r?\n/)
+
+        //Splits each line into Key : Value pair
+        let array = []
+        lines.forEach((line, index) => {
+            if (line !== "" && !line.startsWith(";")){
+
+                console.log(`Line: ${index}, Value: ${line}`)
+                array.push(splitter(line))
+
+
+            }else {
+                console.log("empty String")
+            }
+
+        }, splitter)
+        console.log(array)
+
+
+
+
+    }catch (err){
+        console.error(err)
+    }
+
+
+}
+
+
+ let splitter = function splitRows(row){
+const [key, ...rest] = row.split("=")
+
+    const value = rest.join("=")
+    return[key, value]
+}
+
+/*
+let result = splitRows('MOD_NX_EXPOSER_IMG_CF_PLUGIN_SUPPORT_LBL="Plugin Support"')
+console.log(result)
+
+ */
+
+
+//console.log(splitFilesInRows("upload/mod_nx_exposer_en-GB.ini", splitter))
+
